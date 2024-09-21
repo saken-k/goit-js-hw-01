7bda1a1d63d20bfd4adc4106a570c192e8edcaf8
saken-k <skar.ksk@gmail.com>
2024-09-21 21:18:57 +0300

    hw-01

---
+++ js/task-2.js
@@ -0,0 +1,11 @@
+let country;
+let totalCost;
+let deliveryFee;
+
+function getShippingMessage(country, price, deliveryFee) {
+    return totalCost = `Shipping to ${country} will cost ${price + deliveryFee} credits`
+}
+
+console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
+console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
